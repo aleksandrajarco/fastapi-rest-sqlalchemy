132717bfd834aae09c650f12f7f83874f53ac52a
ajarco <ajarco@chemxon.com>
2020-02-27 13:15:54 +0100

    put and delete methods added

--- app.py
+++ app.py
@@ -35,22 +35,45 @@ engine = create_engine(DATABASE_URL)
 metadata.create_all(engine)
 #metadata.drop_all(engine)
 
+for col in product.c:
+    print(type(col))
+
+# route
+@app.get("/api/v2/hello")
+def get_hello():
+    return {"hello": "world"}
+
 
 @app.get("/product/{id}")
-def get_item(id: int):
-    return {"id": id}
-
+async def get_product(id: int):
+    query = product.select().where(id == product.c.id)
+    return await database.fetch_one(query=query)
 
 @app.get("/product")
 async def get_all():
     query = product.select()
     return await database.fetch_all(query=query)
 
-@app.post("/product")
+@app.post("/product/{id}")
 async def post_product(item: ProductSchema):
     query = product.insert().values(name = item.name, price = item.price, qty = item.qty)
+    return await database.execute(query)
+
+@app.put("/product/{id}")
+async def update_product (id: int, item: ProductSchema):
+    query = (
+        product
+        .update()
+        .where(id == product.c.id)
+        .values(price=item.price, name=item.name, qty=item.qty)
+
+    )
     return await database.execute(query=query)
 
+@app.delete("/product/{id}")
+async def delete_product(id: int ):
+    query = (product.delete().where(id == product.c.id))
+    return await database.execute(query)
 
 if __name__ == '__main__':
     product.create(engine)
